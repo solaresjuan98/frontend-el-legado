bab2e5d59f8004fd03ad4204da964961a9eda1de
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-08-03 01:01:21 -0600

    cambios

--- el-legado-app/src/components/navbar/Navbar.tsx
+++ el-legado-app/src/components/navbar/Navbar.tsx
@@ -6,8 +6,8 @@ import Box from '@mui/joy/Box';
 
 import IconButton from '@mui/joy/IconButton';
 
-//IMAGENES
-import myImage from './Logo_texto.jpg';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/joy/Drawer';
 // Icons import
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
@@ -51,6 +51,11 @@ function ColorSchemeToggle() {
 }
 
 export const Navbar=()=> {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   return (
     <CssVarsProvider disableTransitionOnChange>
@@ -74,33 +79,66 @@ export const Navbar=()=> {
         }}
       >
         <Layout.Header>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 1.5,
-            }}
-          >
-            {/* <img
-            src={".\\el-legado-app\\src\components\navbar\Logo_texto.jpg"}
-            //srcSet={Logo_texto}
-            loading="lazy"
-            alt="legado"
-          /> */}
-          </Box>
-            
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 1.5,
-            }}
-          > 
-          EXPOSITORES
-          
-          </Box>
+        <Box
+  sx={{
+    display: 'flex',
+    flexDirection: ['column', 'row'], // stacking boxes vertically on small screens, horizontally on larger screens
+    alignItems: 'center',
+    justifyContent: 'space-between', // distribute boxes evenly across the main axis
+    p: 1, // add some padding around the Box
+    gap: 1.5, // keep some gap between the Boxes
+  }}
+>
+  <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    }}
+  >
+    <img
+      src={"https://github.com/JuanDiegoAlv1234/ImagenesVarias/blob/master/Logo%20negrita.jpg?raw=true"}
+      loading="lazy"
+      alt="legado"
+      style={{
+        maxWidth: '100%', // ensure the image doesn't go out of the box
+        width: '120px',
+        height: 'auto',
+      }}
+    /> 
+  </Box>
+  <Box sx={{ 
+      display: 'flex', 
+      flexDirection: ['column', 'row'],
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      p: 1, 
+      gap: 1.5,
+    }}>
+  <IconButton 
+        size="sm" 
+        variant="soft" 
+        color="neutral"
+        onClick={toggleDrawer}
+        sx={{ display: { xs: 'block', sm: 'none' } }}
+      >
+        <MenuIcon />
+      </IconButton>
+      
+      <Drawer anchor="left" open={isOpen} onClose={toggleDrawer}>
+        <Box>EXPOSITORES</Box>
+        <Box>TALLERES</Box>
+        <Box>ACERCA DE</Box>
+      </Drawer>
+
+      <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+        <Box>EXPOSITORES</Box>
+        <Box>TALLERES</Box>
+        <Box>ACERCA DE</Box>
+      </Box>
+    </Box>
+</Box>
+
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
             <IconButton
               size="sm"
