3a8ae4091f4e7ccbcd895c474eb8c00d0dd77386
Juan Diego Alvarado <juandiegoalvaradp@gmai.com>
2024-06-26 22:00:14 -0600

    feat: delete from

--- el-legado-app/src/App.tsx
+++ el-legado-app/src/App.tsx
@@ -1,18 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { LandingPage } from "./pages/LandingPage"
 import { RegistroPage } from "./pages/RegistroPage"
-import { Navbar } from "./components/navbar/Navbar"
-import { Talleres } from "./pages/Talleres"
-import { AcercaDe } from "./pages/AcercaDe"
-import { Expositores } from "./pages/Expositores"
-import Footer from "./components/Footer"
-import { Hospedaje } from "./pages/Hospedaje"
+
 import { CssVarsProvider } from "@mui/joy"
 import { ToastContainer } from 'react-toastify';
  
-import { PagoError } from "./pages/PagoError"
 import { AppProvider } from "./context/AppProvider"
-import { ValidacionPago } from "./pages/ValidacionPago"
 function App() {
 
   return (
@@ -21,17 +13,17 @@ function App() {
 
         <BrowserRouter>
           <ToastContainer />
-          <Navbar />
           <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/registro" element={<RegistroPage />} />
-            {/* ====== LANDING PAGE ====== */}
+            <Route path="/" element={<RegistroPage />} />
+          {  /* <Route path="/registro" element={<RegistroPage />} />
+          <Navbar />
+         
             <Route path="/talleres" element={<Talleres />} />
             <Route path="/acerca-de" element={<AcercaDe />} />
             <Route path="/expositores" element={<Expositores />} />
             <Route path="/hospedaje" element={<Hospedaje />} />
 
-            {/* ====== PANTALLAS DE AVISO DE PAGO ====== */}
+           
             <Route path="/validacion" element={<ValidacionPago />} />
       
             <Route path="/pago-error" element={<PagoError />} />
@@ -39,7 +31,8 @@ function App() {
           </Routes>
 
           <br /> <br /> <br /> <br /> <br /> <br />
-          <Footer />
+          <Footer /> */}
+           </Routes>
         </BrowserRouter>
       </CssVarsProvider>
       
